555f408d935b4f475b4e9bfda91867f4c92e8849
Brown, Chris, Vodafone Americas <chris.brown@vodafone.com>
2016-01-19 15:54:37 -0800

    Changed completely to Node

---
+++ main.js
@@ -0,0 +1,31 @@
+const restify = require('restify');
+
+const server = restify.createServer({
+	name:'family-dialer',
+	version:'1.0.0'
+});
+
+server.use(restify.bodyParser());
+
+server.get('/', function(req,res,next){
+	const twiml = '<Response>' +
+					'<Say> Hello, connecting you now </Say>' +
+					'<Gather finishOnKey="#" action="handle-key" method="POST">'+
+					'</Gather>'+
+				'</Response>';
+
+	res.send(200, twiml);
+	next();
+})
+
+server.post('/handle-key', function(req.res.next){
+
+	const twiml = '<Response>'+
+					'<Dial>+'+req.params.Digits+'</Dial>'+
+					'<Say>The call failed or the remote party hung up.  Goodbye.</Say>' + 
+				'</Response>';
+
+	res.send(200, twiml);
+	next();
+})
+

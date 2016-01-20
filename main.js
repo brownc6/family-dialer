902cd7e58d9ec62008eecad9d1e25b28e3976928
Brown, Chris, Vodafone Americas <chris.brown@vodafone.com>
2016-01-19 16:23:50 -0800

    Set header added

--- main.js
+++ main.js
@@ -8,23 +8,26 @@ const server = restify.createServer({
 server.use(restify.bodyParser());
 
 server.post('/', function(req,res,next){
-	const twiml = '<Response>' +
+	const twiml = '<?xml version="1.0" encoding="UTF-8"?>";'+
+				'<Response>' +
 					'<Say> Hello, connecting you now </Say>' +
 					'<Gather finishOnKey="#" action="handle-key" method="POST">'+
 					'</Gather>'+
 				'</Response>';
 
+	res.setHeader('content-type','text/xml');
 	res.send(200, twiml);
 	next();
 })
 
 server.post('/handle-key', function(req,res,next){
-
-	const twiml = '<Response>'+
+	const twiml = '<?xml version="1.0" encoding="UTF-8"?>";'+
+				'<Response>' +
 					'<Dial>+'+req.params.Digits+'</Dial>'+
 					'<Say>The call failed or the remote party hung up.  Goodbye.</Say>' + 
 				'</Response>';
 
+	res.setHeader('content-type','text/xml');
 	res.send(200, twiml);
 	next();
 })

f907f71a3d3d05463053853b592cc82df293506a
Brown, Chris, Vodafone Americas <chris.brown@vodafone.com>
2016-01-19 16:36:58 -0800

    added newline after xml start

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const server = restify.createServer({
 server.use(restify.bodyParser());
 
 server.post('/', function(req,res,next){
-	const twiml = '<?xml version="1.0" encoding="UTF-8" ?>'+
+	const twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n'+
 				'<Response>' +
 					'<Say> Hello, connecting you now </Say>' +
 					'<Gather finishOnKey="#" action="handle-key" method="POST">'+
@@ -21,7 +21,7 @@ server.post('/', function(req,res,next){
 })
 
 server.post('/handle-key', function(req,res,next){
-	const twiml = '<?xml version="1.0" encoding="UTF-8"?>'+
+	const twiml = '<?xml version="1.0" encoding="UTF-8"?>\n'+
 				'<Response>' +
 					'<Dial>+'+req.params.Digits+'</Dial>'+
 					'<Say>The call failed or the remote party hung up.  Goodbye.</Say>' + 
